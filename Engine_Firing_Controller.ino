77fc75193fe1e38124cb728ef879a93b692d75d7
ElliottBG <esb20@ic.ac.uk>
2023-06-10 19:57:51 +0100

    Working on cold flow

--- Engine_Firing_Controller.ino
+++ Engine_Firing_Controller.ino
@@ -18,6 +18,7 @@ int servo1_pin = 6;    //fuel
 int servo2_pin = 5;    //oxidiser
 
 int pyro1_pin  = 10;
+
 int pyro2_pin  = 11;
 
 int engine_fire = A1;
@@ -68,75 +69,76 @@ void setup() {
   digitalWrite(pyro1_pin, LOW);
   digitalWrite(pyro2_pin, LOW);  
   
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  // Open //Serial communications and wait for port to open:
+  //Serial.begin(9600);
+//  while (!//Serial) {
+//    ; // wait for //Serial port to connect. Needed for native USB port only
+//  }
 
-  Serial.print("\nInitializing SD card...");
+  //Serial.print("\nInitializing SD card...");
 
   // we'll use the initialization code from the utility libraries
   // since we're just testing if the card is working!
   if (!card.init(SPI_HALF_SPEED, chipSelect)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card inserted?");
-    Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    //Serial.println("initialization failed. Things to check:");
+    //Serial.println("* is a card inserted?");
+    //Serial.println("* is your wiring correct?");
+    //Serial.println("* did you change the chipSelect pin to match your shield or module?");
     while (1);
   } else {
-    Serial.println("Wiring is correct and a card is present.");
+    //Serial.println("Wiring is correct and a card is present.");
   }
 
   // print the type of card
-  Serial.println();
-  Serial.print("Card type:         ");
+  //Serial.println();
+  //Serial.print("Card type:         ");
   switch (card.type()) {
     case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
+      //Serial.println("SD1");
       break;
     case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
+      //Serial.println("SD2");
       break;
     case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
+      //Serial.println("SDHC");
       break;
     default:
-      Serial.println("Unknown");
+      //Serial.println("Unknown");
+      break;
   }
 
   // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
   if (!volume.init(card)) {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    //Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
     while (1);
   }
 
-  Serial.print("Clusters:          ");
-  Serial.println(volume.clusterCount());
-  Serial.print("Blocks x Cluster:  ");
-  Serial.println(volume.blocksPerCluster());
+  //Serial.print("Clusters:          ");
+  //Serial.println(volume.clusterCount());
+  //Serial.print("Blocks x Cluster:  ");
+  //Serial.println(volume.blocksPerCluster());
 
-  Serial.print("Total Blocks:      ");
-  Serial.println(volume.blocksPerCluster() * volume.clusterCount());
-  Serial.println();
+  //Serial.print("Total Blocks:      ");
+  //Serial.println(volume.blocksPerCluster() * volume.clusterCount());
+  //Serial.println();
 
   // print the type and size of the first FAT-type volume
   uint32_t volumesize;
-  Serial.print("Volume type is:    FAT");
-  Serial.println(volume.fatType(), DEC);
+  //Serial.print("Volume type is:    FAT");
+  //Serial.println(volume.fatType(), DEC);
 
   volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
   volumesize *= volume.clusterCount();       // we'll have a lot of clusters
   volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
-  Serial.print("Volume size (Kb):  ");
-  Serial.println(volumesize);
-  Serial.print("Volume size (Mb):  ");
+  //Serial.print("Volume size (Kb):  ");
+  //Serial.println(volumesize);
+  //Serial.print("Volume size (Mb):  ");
   volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.print("Volume size (Gb):  ");
-  Serial.println((float)volumesize / 1024.0);
+  //Serial.println(volumesize);
+  //Serial.print("Volume size (Gb):  ");
+  //Serial.println((float)volumesize / 1024.0);
 
-  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+  //Serial.println("\nFiles found on the card (name, date and size in bytes): ");
   root.openRoot(volume);
 
   // list all files in the card with date and size
@@ -146,29 +148,22 @@ void setup() {
 void loop() {
   
   if (digitalRead(engine_fire) == HIGH){
-    myFile = SD.open("EngineConrollerData.txt", FILE_WRITE);
     delay(100);
+    myFile = SD.open("EngineConrollerData.txt", FILE_WRITE);
     long start_time = millis();
     
-    if (myFile) {
-         Serial.print("Writing to test.txt...");
-         myFile.println("testing 1, 2, 3.");
-         // close the file:
-         myFile.close();
-        Serial.println("done.");
-  } else {
-      // if the file didn't open, print an error:
-      Serial.println("error opening test.txt");
-  }
     // ignites both pyro channels
     while((digitalRead(engine_fire) == HIGH) && (millis() - start_time < 500)){
       digitalWrite(pyro1_pin, HIGH);
       digitalWrite(pyro2_pin, HIGH);
+
+
+      
       chamberP = (analogRead(chamber_pressure_pin)*ptcalgrad)+ptcalconst;         //returns chamber pressure in bar
       fuelP = (analogRead(fuel_injector_pressure_pin)*ptcalgrad)+ptcalconst;      //returns fuel pressure in bar
       dataString = String(millis()) + "," + String(chamberP) + "," + String(fuelP) + "," + String(demandFuelP) + "," + String(FuelServoAngleDemand);
-      Serial.println(dataString);
-      myFile.println(1);
+      //Serial.println(dataString);
+      myFile.println(dataString);
 
     }
 
@@ -177,11 +172,14 @@ void loop() {
       
       fuelServo.write(60);
       oxidiserServo.write(90);
+
+
+      
       chamberP = (analogRead(chamber_pressure_pin)*ptcalgrad)+ptcalconst;         //returns chamber pressure in bar
       fuelP = (analogRead(fuel_injector_pressure_pin)*ptcalgrad)+ptcalconst;      //returns fuel pressure in bar
       dataString = String(millis()) + "," + String(chamberP) + "," + String(fuelP) + "," + String(demandFuelP) + "," + String(FuelServoAngleDemand);
-      Serial.println(dataString);
-      myFile.println(1);
+      //Serial.println(dataString);
+      myFile.println(dataString);
 
     }
 
@@ -190,11 +188,14 @@ void loop() {
 
       fuelServo.write(185);
       oxidiserServo.write(185);
+
+
+      
       chamberP = (analogRead(chamber_pressure_pin)*ptcalgrad)+ptcalconst;         //returns chamber pressure in bar
       fuelP = (analogRead(fuel_injector_pressure_pin)*ptcalgrad)+ptcalconst;      //returns fuel pressure in bar
       dataString = String(millis()) + "," + String(chamberP) + "," + String(fuelP) + "," + String(demandFuelP) + "," + String(FuelServoAngleDemand);
-      Serial.println(dataString);
-      myFile.println(1);
+      //Serial.println(dataString);
+      myFile.println(dataString);
 
     }
 
@@ -205,9 +206,9 @@ void loop() {
     while((digitalRead(engine_fire) == HIGH) && (millis() - start_time < 10000)){ 
 
       chamberP = (analogRead(chamber_pressure_pin)*ptcalgrad)+ptcalconst;         //returns chamber pressure in bar
-      fuelP = (analogRead(fuel_injector_pressure_pin)*ptcalgrad)+ptcalconst;      //returns fuel pressure in bar
-      //Serial.println(analogRead(fuel_injector_pressure_pin));
-      if ((chamberP > 33) && (chamberP < 27) && (fuelP > 43) && (fuelP < 37)){  //condition for abnormal operation, can make them tighter or wider
+      fuelP = ((analogRead(fuel_injector_pressure_pin)*ptcalgrad)+ptcalconst);      //returns fuel pressure in bar
+      ////Serial.println(analogRead(fuel_injector_pressure_pin));
+      if (chamberP > 2){  //(chamberP > 23) && (chamberP < 17) && (fuelP < 30) && (fuelP > 50)){  //condition for abnormal operation, can make them tighter or wider
         demandFuelP = 0.016 * pow(chamberP,2) + chamberP;    //function relating demanded fuel pressure to chamber pressure
         error = demandFuelP - fuelP;
         FuelServoAngleDemand = FuelServoAnglePrev + Kp * error;        //Any PID controller may work
@@ -226,9 +227,11 @@ void loop() {
       }
       
       delay(50); //delay to give the servo chance to start moving
+
+      
       dataString = String(millis()) + "," + String(chamberP) + "," + String(fuelP) + "," + String(demandFuelP) + "," + String(FuelServoAngleDemand);
-      Serial.println(dataString);
-      myFile.println(1);
+      //Serial.println(dataString);
+      myFile.println(dataString);
     }
 
     
